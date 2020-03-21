51cc0142ceec5b77532997b6763193ec3a44c7dd
Zeyu Dong <dzy201415@gmail.com>
2020-03-21 12:11:25 +0800

    add cache for auto translator

--- api/auto.py
+++ api/auto.py
@@ -2,11 +2,22 @@ import google as google
 import youdao as youdao
 from exceptions import TranslateException
 
+cache = {}
+cache_size = 5000
+
 
 def get_trans(word):
+    if word in cache:
+        return cache[word]
+
     try:
         trans = youdao.get_trans(word, False)
     except TranslateException:
         trans = google.get_trans(word)
+
+    if len(cache) > cache_size:
+        cache.pop(cache.__iter__().__next__())
+
+    cache[word] = trans
 
     return trans
