8c693cc12e56a8447fe6a91458a44a2ed12ad392
Zeyu Dong <dzy201415@gmail.com>
2020-03-21 12:46:58 +0800

    update youdao translate

--- api/youdao.py
+++ api/youdao.py
@@ -1,5 +1,4 @@
 import logging
-import time
 from exceptions import TranslateException
 from urllib.parse import quote
 
@@ -9,6 +8,7 @@ from bs4 import BeautifulSoup
 session = requests.session()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 
 def get_trans(word, machine=True):
     try:
@@ -21,27 +21,37 @@ def get_trans(word, machine=True):
                         f"https://www.youdao.com/w/{quote(word)}/",
                         timeout=2) as re:
                     if re.status_code == 200:
-                        content = BeautifulSoup(re.content.decode(),
-                                                "html5lib")
-                        trans = content.find(attrs={"id": "phrsListTab"})
+                        content = BeautifulSoup(re.content.decode(), "html5lib")
+                        trans = content.find(id="phrsListTab")
+                        trans = trans.find(attrs={
+                            "class": "trans-container"
+                        }) if trans else None
+                        trans = trans.find_all("li") if trans else None
+
                         if trans:
-                            trans = trans.find(attrs={
-                                "class": "trans-container"
-                            }).find_all("li")
-                            if trans:
-                                trans = [i.text for i in trans]
-                            else:
-                                raise TranslateException(
-                                    "No translation found")
+                            return [i.text for i in trans]
                         elif machine:
-                            trans = [
-                                content.find(attrs={
-                                    "id": "fanyiToggle"
-                                }).find_all("p")[1].text
-                            ]
-                        else:
-                            raise TranslateException("No translation found")
-                        return trans
+                            # Try "网络释义" first
+                            trans = content.find(id="tWebTrans")
+                            trans = trans.find_all(attrs={
+                                "class": "wt-container"
+                            }) if trans else None
+
+                            if trans:
+                                return [
+                                    i.find(attrs={
+                                        "class": "title"
+                                    }).find("span").text for i in trans
+                                ]
+
+                            # Try "机器翻译" then
+                            trans = content.find(id="fanyiToggle")
+                            if trans:
+                                return [
+                                    trans.find_all("p")[1].text
+                                ]
+
+                        raise TranslateException("No translation found")
             except requests.RequestException:
                 count = count - 1
                 logger.warn(
