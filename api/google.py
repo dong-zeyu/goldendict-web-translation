28bfd1caa970b7cb7f1f45da5654742103ae8669
Zeyu Dong <dzy201415@gmail.com>
2020-12-13 21:56:59 -0500

    port googletran new version

--- api/google.py
+++ api/google.py
@@ -8,7 +8,6 @@ from ..exceptions import TranslateException
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 translator = Translator(
-    service_urls=["translate.google.cn"],
     user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
     timeout=5)
 
@@ -21,13 +20,7 @@ def get_trans(word, machine=True):
         else:
             trans = translator.translate(word, dest="zh-CN")
 
-        data = trans.extra_data
-        if data["all-translations"]:
-            trans = [
-                f"{i[0]}: {'; '.join(i[1])}" for i in data["all-translations"]
-            ]
-        else:
-            trans = [trans.text]
+        trans = [trans.text]
 
         return trans
     except requests.RequestException:
